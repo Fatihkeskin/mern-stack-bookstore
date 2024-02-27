da544a31da52e2baada6981edc8f0b6a5e12407f
fatihkeskin <fatihkeskin7@outlook.com>
2024-02-27 16:00:38 +0100

    frontend initialized, books route refactored

--- backend/index.js
+++ backend/index.js
@@ -1,61 +1,26 @@
 import express, { request } from "express";
 import { PORT, mongoDBURL } from "./config.js"
 import mongoose from 'mongoose';
-import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000/',
+        methods: ['GET' , 'PUT', 'POST', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+}));
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome')
 });
 
-app.post('/books', async (request, response) => {
-    try {
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-        const book = await Book.create(newBook);
-
-         return response.status(201).send(book);
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send( { message: error.message });
-    }
-});
-
-app.get('/books', async (request, response) => {
-    try {
-        const books = await Book.find({});
-
-        return response.status(200).json({
-            count: books.length,
-            data: books
-        })
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send( { message: error.message });
-    }
-})
-
-app.get('/books/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
-
-        const book = await Book.findById(id);
-
-        return response.status(200).json({
-            book
-        })
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send( { message: error.message });
-    }
-})
+app.use('/books', booksRoute);
 
 mongoose.connect(mongoDBURL)
 .then(() => {
